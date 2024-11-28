1c1e10d3064cc5401492f57ce0c9f225a0bc400b
jesus mendoza <jesusdavidmv02@gmail.com>
2024-11-27 21:47:28 -0500

    flix

--- swaggerConfig.ts
+++ swaggerConfig.ts
@@ -12,7 +12,8 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: "http://localhost:3000/api/v1/", // URL base de la API
+        // url: "http://localhost:3000/api/v1/",
+         url: "https://despliege-everyonefilies-production.up.railway.app/api-docs/api/v1/" // URL base de la API
       },
     ],
   }, 
